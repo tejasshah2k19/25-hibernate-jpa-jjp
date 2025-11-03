a2892202ca4f02ab4bc8bc7f22eb3580e5cde67a
Tejas Shah <tejasshah2k19@gmail.com>
2025-11-03 13:51:59 +0530

    find by first name

--- src/main/java/com/repository/StudentRepository.java
+++ src/main/java/com/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.entity.StudentEntity;
 
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
+	
+	List<StudentEntity> findByFirstName(String firstName);
+	
 }
